699eb115579e239f91fb8e64222e3d699bdf6211
oussama <bz.oussama@gmail.com>
2020-12-28 22:13:06 +0100

    added disqus

---
+++ src/components/disqus.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { useStaticQuery } from "gatsby"
+import { Disqus, CommentCount } from "gatsby-plugin-disqus"
+
+const DiscusArea = ({ id, title, location }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          siteUrl
+        }
+      }
+    }
+  `)
+  const siteUrl = data.site.siteMetadata?.siteUrl
+  let disqusConfig = {
+    url: `${siteUrl + location.pathname}`,
+    identifier: id,
+    title: title,
+  }
+
+  return (
+    <>
+      <CommentCount config={disqusConfig} placeholder={"..."} />
+      <Disqus config={disqusConfig} />
+    </>
+  )
+}
+
+export default DiscusArea
