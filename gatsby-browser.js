0db3bc1464cde4fbc161ee2f81178691bb3ef6ee
oussama <bz.oussama@gmail.com>
2021-06-22 14:13:51 +0100

    initial observerIntersection added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,9 @@ import "prismjs/themes/prism.css"
 import "./src/css/index.css"
 // custom CSS styles
 import "./src/style.css"
+
+var smoothscroll = require("smoothscroll-polyfill")
+
+export function onClientEntry() {
+  smoothscroll.polyfill()
+}
