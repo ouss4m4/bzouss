bdd4c17d8fde29ca2c1d7711ba201359c2c3cdcd
oussama <bz.oussama@gmail.com>
2021-06-22 11:34:11 +0100

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
