aa455e96a9aab0a84dd694cc249b908ae4d831a0
oussama <bz.oussama@gmail.com>
2021-06-26 19:16:22 +0100

    initial tools added

--- src/components/portfolio/Tools.js
+++ src/components/portfolio/Tools.js
@@ -1,23 +1,53 @@
 import React from "react"
+import FluidIMG from "../../images/FluidIMG"
 
 export const Tools = () => {
   return (
     <div className="container">
       <h1>Tools</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate animi
-        soluta quasi atque laudantium possimus cum maxime obcaecati illo
-        deleniti exercitationem at facere dolorum, excepturi, incidunt aliquam
-        voluptatum voluptas alias.
-      </p>
-      <hr />
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate animi
-        soluta quasi atque laudantium possimus cum maxime obcaecati illo
-        deleniti exercitationem at facere dolorum, excepturi, incidunt aliquam
-        voluptatum voluptas alias.
-      </p>
+      <div className="grid text-center">
+        <p>FrontEnd</p>
+        <div className="flex gap-4 justify-center">
+          <div className="flex-col ">
+            <FluidIMG filename="angular-log" />
+            <span>Angular</span>
+          </div>
+          <div className="flex-col ">
+            <FluidIMG filename="react-log" />
+            <span>React</span>
+          </div>
+          <span>Gatsby</span>
+          <span>Typescript</span>
+        </div>
+        <p>BackEnd</p>
+        <div className="flex gap-4 justify-center">
+          <span>Node</span>
+          <span>Express</span>
+          <span>MongoDB</span>
+          <span>Typescript</span>
+        </div>
+        <p>Mobile Apps</p>
+        <div className="flex gap-4 justify-center">
+          <span>NativeScript</span>
+          <span>Flutter</span>
+        </div>
+        <p>Desktop Apps</p>
+        <div className="flex gap-4 justify-center">
+          <span>Electron</span>
+        </div>
+        <p>DevOps</p>
+        <div className="flex gap-4 justify-center">
+          <span>Firebase</span>
+          <span>Netlify</span>
+          <span>Amazon</span>
+        </div>
+        <p>DevTools</p>
+        <div className="flex gap-4 justify-center">
+          <span>Git</span>
+          <span>Webpack</span>
+          <span>Rsync</span>
+        </div>
+      </div>
     </div>
   )
 }
